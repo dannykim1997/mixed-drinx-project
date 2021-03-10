903112015166c97c035aa44e5e679a0b098af729
Larry Hawelu <lhawelu@gmail.com>
2021-03-10 10:55:39 -0800

    updating JS to handle null values

--- index.js
+++ index.js
@@ -40,27 +40,45 @@ const showCardInfo = function(drinks) {
 	cardInfo.innerHTML = ""
 	
 	const glass = document.createElement("h5")
+  const directions = document.createElement("h6")
+  const iba = document.createElement("h7")
+  const ing1 = document.createElement("ul")
+  const ing2 = document.createElement("ul")
+  const ing3 = document.createElement("ul")
+  const ing4 = document.createElement("ul")
+  const ing5 = document.createElement("ul")
+  const ing6 = document.createElement("ul")
+  const ing7 = document.createElement("ul")
+  
 	glass.textContent = `Glassware in image: ${drinks.strGlass}`
-	const directions = document.createElement("h6")
+	
 	directions.textContent = `Instructions: ${drinks.strInstructions}"`
-	const iba = document.createElement("h7")
+	
 	iba.textContent = `IBA Category: ${drinks.strIBA}`
-	const ing1 = document.createElement("ul")
-	ing1.textContent = `${drinks.strIngredient1} - ${drinks.strMeasure1}`
-	const ing2 = document.createElement("ul")
-	ing2.textContent = `${drinks.strIngredient2} - ${drinks.strMeasure2}`
-	const ing3 = document.createElement("ul")
-	ing3.textContent = `${drinks.strIngredient3} - ${drinks.strMeasure3}`
-	const ing4 = document.createElement("ul")
-	ing4.textContent = `${drinks.strIngredient4} - ${drinks.strMeasure4}`
-	const ing5 = document.createElement("ul")
-	ing5.textContent = `${drinks.strIngredient5} - ${drinks.strMeasure5}`
-	const ing6 = document.createElement("ul")
-	ing6.textContent = `${drinks.strIngredient6} - ${drinks.strMeasure6}`
-	const ing7 = document.createElement("ul")
-	ing7.textContent = `${drinks.strIngredient7} - ${drinks.strMeasure7}`
+	
+  ing1.textContent = setTextContent(drinks.strIngredient1, drinks.strMeasure1)
+	
+  ing2.textContent = setTextContent(drinks.strIngredient2, drinks.strMeasure2)
+	
+  ing3.textContent = setTextContent(drinks.strIngredient3, drinks.strMeasure3)
+	
+  ing4.textContent = setTextContent(drinks.strIngredient4, drinks.strMeasure4)
+	
+  ing5.textContent = setTextContent(drinks.strIngredient5, drinks.strMeasure5)
+	
+  ing6.textContent = setTextContent(drinks.strIngredient6, drinks.strMeasure6)
+	
+	ing7.textContent = setTextContent(drinks.strIngredient7, drinks.strMeasure7)
 	
 	cardInfo.append(glass, directions, iba, ing1, ing2, ing3, ing4, ing5, ing6, ing7)
 	container.appendChild(cardInfo)
 	}
+  
+  function setTextContent(ingredients, measurements) {
+    if (ingredients === null) {
+      return ""
+    } else {
+      return `${ingredients} - ${measurements}`
+    }
+  }
 	// showCardInfo.className = "moreInfo"
